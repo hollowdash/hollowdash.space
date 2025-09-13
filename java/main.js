2da16d09807f516d9514e516255675310a58d2fa
Bryson <bryson.dev2011@gmail.com>
2025-09-13 17:16:02 -0400

    Delete java directory

--- java/main.js
+++
@@ -1,7 +0,0 @@
-const menuBtn = document.getElementById('menuBtn');
-const menuOverlay = document.getElementById('menuOverlay');
-
-menuBtn.addEventListener('click', () => {
-    menuOverlay.classList.toggle('open');
-    menuBtn.classList.toggle('active'); // triggers animation
-});
